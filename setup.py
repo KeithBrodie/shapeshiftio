d82e6249e8fe3860cc8a052ce6ad36dc09924273
Keith Brodie <keith.brodie@gmail.com>
2017-08-04 19:30:45 -0700

    Update Setup.py
    Changed source URL to this fork.

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 setup(name='shapeshiftio',
         version='0.1.1',
         description='Python API for https://shapeshift.io',
-        url='http://github.com/abitfan/shapeshiftio',
-        author='A Bit Fan',
-        author_email='abitfan@ruggedinbox.com',
+        url='https://github.com/KeithBrodie/shapeshiftio',
+        author='Another Bit Fan',
+        author_email='no.thanks@nowhere.com',
         license='MIT',
         packages=['shapeshiftio'],
         zip_safe=False)
