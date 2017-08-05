4442d8d63b849d1bcfb4b4fd90ec306be4fe1a3f
Keith Brodie <keith.brodie@gmail.com>
2017-08-04 19:43:14 -0700

    Update version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='shapeshiftio',
-        version='1.0.0',
+        version='1.0.1',
         description='Python API for https://shapeshift.io',
         url='https://github.com/KeithBrodie/shapeshiftio',
         author='Another Bit Fan',
